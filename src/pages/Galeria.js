ca44e3a60115193f67be7d6f960ac8494144563e
Luis27cc <luiscampodel@gmail.com>
2025-02-17 01:53:30 +0100

    Update Galeria.js

--- src/pages/Galeria.js
+++ src/pages/Galeria.js
@@ -6,7 +6,7 @@ function Galeria() {
       <h1>Galería de Fotos</h1>
       <p>Historias de autoaceptación y belleza real.</p>
       <a href="https://drive.google.com/drive/folders/1GrngXu_UpeEKFjMA9itSzT0g0p0eEkF-?usp=drive_link" target="_blank" rel="noopener noreferrer">
-        <img src="https://source.unsplash.com/400x300/?portrait" alt="Autoaceptación" />
+        <button>Autoaceptación</button>
       </a>
     </div>
   );
