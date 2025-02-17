64ce9a04991fc651f42a422aa58c797bed2c4d5c
Luis27cc <luiscampodel@gmail.com>
2025-02-17 01:21:15 +0100

    Update TestAutoestima.js

--- src/pages/TestAutoestima.js
+++ src/pages/TestAutoestima.js
@@ -1,7 +1,13 @@
 import React from "react";
 
 function TestAutoestima() {
-  return <h1>Test de Autoestima</h1>;
+  return (
+    <div>
+      <h1>Test de Autoestima</h1>
+      <p>Responde estas preguntas para evaluar tu nivel de autoestima.</p>
+      <button>Iniciar Test</button>
+    </div>
+  );
 }
 
 export default TestAutoestima;
