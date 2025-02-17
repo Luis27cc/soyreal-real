2100a3a95a3b9cc9297a2a596b0dda8aced9f36d
Luis27cc <luiscampodel@gmail.com>
2025-02-17 01:47:20 +0100

    Update Foro.js

--- src/pages/Foro.js
+++ src/pages/Foro.js
@@ -5,6 +5,9 @@ function Foro() {
     <div>
       <h1>Foro de Autoestima</h1>
       <p>Comparte experiencias y apoya a otros en su camino de autoaceptación.</p>
+      <a href="https://drive.google.com/drive/folders/1c_rpBXpNfsLmH7a3GKcrRKTmaO7C5yKw?usp=drive_link" target="_blank" rel="noopener noreferrer">
+        <button>Comparte</button>
+      </a>
     </div>
   );
 }
