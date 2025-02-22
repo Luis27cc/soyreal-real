8e739569db75eaa72d0692fe667ab06bf72f7c87
Luis27cc <luiscampodel@gmail.com>
2025-02-22 23:34:04 +0100

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./styles.css"; // Asegúrate de que este import esté en tu archivo
+import "./styles.css"; // Importamos los estilos correctamente
 
 function Home() {
   return (
