81538930b82bd5772d7c22ab44b3643179814900
Luis27cc <luiscampodel@gmail.com>
2025-02-23 00:39:46 +0100

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,23 @@
 import React from "react";
-import "../styles.css"; // Asegura la importación correcta
 
 function Home() {
+  const backgroundStyle = {
+    backgroundImage: "url('https://i.postimg.cc/50dWXJns/fondo-final.jpg')",
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+    minHeight: "100vh",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    textAlign: "center",
+    color: "white",
+    padding: "20px",
+  };
+
   return (
-    <div className="container">
+    <div style={backgroundStyle}>
       <h1>Bienvenido a #SoyReal</h1>
       <p>Un espacio para promover la autoestima y la autoaceptación.</p>
       <a href="https://drive.google.com/drive/folders/1l12zJYch3312E1tzOQEFi2dwV3cWM66K?usp=drive_link" target="_blank" rel="noopener noreferrer">
