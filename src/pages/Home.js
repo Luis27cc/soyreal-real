9749ae025b5362a3ef2a661594e8bac057c3e4a1
Luis27cc <luiscampodel@gmail.com>
2025-02-23 01:07:20 +0100

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,19 @@
 import React from "react";
-import background from "../assets/background.jpg"; // Ruta relativa a la imagen
-import "./styles.css"; // Importamos los estilos
+import "./styles.css";
 
 function Home() {
   return (
-    <div className="home-container" style={{ backgroundImage: `url(${background})` }}>
+    <div
+      className="home-container"
+      style={{ backgroundImage: "url(./assets/background.jpg)" }}
+    >
       <h1>Bienvenido a #SoyReal</h1>
       <p>Un espacio para promover la autoestima y la autoaceptación.</p>
-      <a href="https://drive.google.com/drive/folders/1l12zJYch3312E1tzOQEFi2dwV3cWM66K?usp=drive_link" target="_blank" rel="noopener noreferrer">
+      <a
+        href="https://drive.google.com/drive/folders/1l12zJYch3312E1tzOQEFi2dwV3cWM66K?usp=drive_link"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         <button>Sonrisa</button>
       </a>
     </div>
